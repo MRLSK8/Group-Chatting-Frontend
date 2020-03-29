33e1faa1686dff9f28bf42d5e12724f20ab28859
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-29 19:37:07 -0300

    Server socket join and sendMessage done.

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,14 +7,42 @@ const cors = require('cors');
 const PORT = process.env.PORT || 3333;
 const routes = require('./router');
 
+const {
+  addUser,
+  getUser,
+  getUsersInRoom,
+  removeUser
+} = require('./utils/users');
+
 app.use(cors());
 app.use(express.json());
 
 socket_io.on('connection', socket => {
-  console.log('We have a new connection!');
-
   socket.on('join', ({ name, room }, callback) => {
-    console.log(name, room);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit('message', {
+      user: 'admin',
+      text: `${user.name}, welcome to the room ${user.room}.`
+    });
+
+    socket.broadcast
+      .to(user.room)
+      .emit('message', { user: 'admin', text: `${user.name}, has joing.` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    socket_io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
   });
 
   socket.on('disconnect', () => {
