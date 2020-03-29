de28e103f8b4b80d6e52410e271b19b199aa8dff
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-29 15:34:16 -0300

    Routes done and server running with socke.io.

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const socket_io = require('socket.io')(server);
+
+const cors = require('cors');
+const PORT = process.env.PORT || 3333;
+const routes = require('./router');
+
+app.use(cors());
+app.use(express.json());
+
+socket_io.on('connection', socket => {
+  console.log('We have a new connection!');
+
+  socket_io.on('disconnect', () => {
+    console.log('User has left!');
+  });
+});
+
+app.use(routes);
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
