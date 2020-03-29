9392d752f97a6b30b953a82982c29de1374db298
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-03-29 16:01:34 -0300

    Login page done (without css for now)

--- frontend/src/Pages/Login/index.js
+++ frontend/src/Pages/Login/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 // import { Container } from './styles';
 
 export default function Login() {
-  return <h1>Login</h1>;
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return (
+    <div>
+      <div>
+        <h1>Join</h1>
+        <div>
+          <input
+            placeholder='Name'
+            onChange={event => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder='Room'
+            onChange={event => setRoom(event.target.value)}
+          />
+        </div>
+        <Link
+          onClick={event => (!name || !room ? event.preventDefault() : null)}
+          to={`/chat?name${name}&room=${room}`}
+        >
+          <button type='submit'>Sign In</button>
+        </Link>
+      </div>
+    </div>
+  );
 }
