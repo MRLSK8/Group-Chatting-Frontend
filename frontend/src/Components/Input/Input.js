c6837fab579a83a442930138b4e2d6873e6da075
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-04-02 20:48:00 -0300

    Reducer added

--- frontend/src/Components/Input/Input.js
+++ frontend/src/Components/Input/Input.js
@@ -2,22 +2,25 @@ import React, { useContext } from 'react';
 import MyContext from '../../Contexts/Context';
 
 export default function Input() {
-  const { message, sendMessages, setMessage } = useContext(MyContext);
+  const { message, sendMessages, dispatch } = useContext(MyContext);
 
   return (
     <form>
-      <input
-        type='text'
-        placeholder='Type a message...'
-        value={message}
-        onKeyPress={event =>
-          event.key === 'Enter' ? sendMessages(event) : null
-        }
-        onChange={event => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={event => sendMessages(event)}>Send</button>
-    </form>
+    <input
+      type='text'
+      placeholder='Type a message...'
+      value={message}
+      onKeyPress={event =>
+        event.key === 'Enter' ? sendMessages(event) : null
+      }
+      onChange={event => {
+        dispatch({
+          type: 'MESSAGE',
+          payload: { message: event.target.value }
+        });
+      }}
+    />
+    <button onClick={event => sendMessages(event)}>Send</button>
+  </form>
   );
 }
--- frontend/src/Pages/Chat/index.js
+++ frontend/src/Pages/Chat/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import queryString from 'query-string';
 import { useLocation, useHistory } from 'react-router-dom';
 
@@ -13,12 +13,35 @@ import io from 'socket.io-client';
 const ENDPOINT = 'localhost:3333';
 let socket;
 
+const INITIAL_STATE = {
+  usersInRoom: [],
+  messages: [],
+  userName: '',
+  roomName: '',
+  message: ''
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'USERNAME_AND_ROOM':
+      return {
+        ...state,
+        userName: action.payload.userName,
+        roomName: action.payload.roomName
+      };
+    case 'USERS_IN_ROOM':
+      return { ...state, usersInRoom: action.payload.usersInRoom };
+    case 'MESSAGE':
+      return { ...state, message: action.payload.message };
+    case 'MESSAGES':
+      return { ...state, messages: action.payload.messages };
+    default:
+      return state;
+  }
+};
+
 export default function Chat() {
-  const [usersInRoom, setUsersInRoom] = useState([]);
-  const [userName, setUserName] = useState('');
-  const [roomName, setRoomName] = useState('');
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState('');
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
 
   const location = useLocation();
   const history = useHistory();
@@ -26,8 +49,10 @@ export default function Chat() {
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    setUserName(name);
-    setRoomName(room);
+    dispatch({
+      type: 'USERNAME_AND_ROOM',
+      payload: { userName: name, roomName: room }
+    });
 
     socket = io(ENDPOINT);
 
@@ -39,7 +64,7 @@ export default function Chat() {
     });
 
     socket.on('roomData', users => {
-      setUsersInRoom(users);
+      dispatch({ type: 'USERS_IN_ROOM', payload: { usersInRoom: users } });
     });
 
     return () => {
@@ -50,30 +75,35 @@ export default function Chat() {
 
   useEffect(() => {
     socket.on('message', message => {
-      setMessages([...messages, message]);
+      dispatch({
+        type: 'MESSAGES',
+        payload: { messages: [...state.messages, message] }
+      });
     });
 
     socket.on('roomData', users => {
-      setUsersInRoom(users);
+      dispatch({ type: 'USERS_IN_ROOM', payload: { usersInRoom: users } });
     });
-  }, [messages]);
+  }, [state.messages]);
 
   const sendMessages = event => {
     event.preventDefault();
 
-    if (message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
+    if (state.message) {
+      socket.emit('sendMessage', state.message, () =>
+        dispatch({ type: 'MESSAGE', payload: { message: '' } })
+      );
     }
   };
 
   const allData = {
-    messages,
-    message,
-    userName,
-    roomName,
+    userName: state.userName,
+    roomName: state.roomName,
+    usersInRoom: state.usersInRoom,
+    messages: state.messages,
+    message: state.message,
     sendMessages,
-    setMessage,
-    usersInRoom
+    dispatch
   };
 
   return (
