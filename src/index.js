e80464e66e4e0b891755ba8dc02cd2130bdd5345
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-04-29 21:57:48 -0300

    Test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,4 +9,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
