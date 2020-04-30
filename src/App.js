dab3f67b6b983cb07dfdf3307c597d02af5623a0
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-04-29 21:47:54 -0300

    Service Worker registered

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyles from './styles';
+import * as serviceWorker from './serviceWorker';
 
 export default function App() {
   return (
@@ -11,3 +12,5 @@ export default function App() {
     </Router>
   );
 }
+
+serviceWorker.register();
