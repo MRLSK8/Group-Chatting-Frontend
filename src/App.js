e423b2f063e68107789e072a7760f94da1b81350
Marcelo Lima <marcelo_sp5@hotmail.com>
2020-04-29 21:54:22 -0300

    Service worker on the right place

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './routes';
 import GlobalStyles from './styles';
-import * as serviceWorker from './serviceWorker';
 
 export default function App() {
   return (
@@ -12,5 +11,3 @@ export default function App() {
     </Router>
   );
 }
-
-serviceWorker.register();
